0ed4bee2092183ddf90dd9f2475fae72551e3596
edinson <dariosg76@gmail.com>
2026-02-24 01:22:23 -0500

    correcciones

--- app/src/main/java/com/example/helloandroidsuarezedinson/MainActivity.kt
+++ app/src/main/java/com/example/helloandroidsuarezedinson/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    // 1. Variable para el contador
     private var contador = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,23 +18,19 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Configuración de bordes (ya la tenías)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        // 2. Referencias a los elementos de la interfaz
         val tvSaludo: TextView = findViewById(R.id.txtContador)
         val btnSaludar: Button = findViewById(R.id.btnSaludar)
 
-        // 3. Acción del botón
         btnSaludar.setOnClickListener {
-            contador++ // Aumenta el contador
-            tvSaludo.text = "¡Hola Android! Clics: $contador" // Actualiza el texto
+            contador++
+            tvSaludo.text = "¡Hola Android! Clics: $contador"
 
-            // Muestra el mensaje flotante (Toast)
             Toast.makeText(this, "¡Botón presionado!", Toast.LENGTH_SHORT).show()
         }
     }
